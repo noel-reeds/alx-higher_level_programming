ecd9634138bd62c39aae32ec8c9400d40536c330
Noel Reeds <noelreeds01@gmail.com>
2023-11-28 22:08:50 +0000

    fixed last digit issue for -ve number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-mod = abs(number) % 10
+
+if number < 0:
+    mod = -(abs(number) % 10)
+else:
+    mod = number % 10
+
 if mod > 5:
     print(f"Last digit of {number:d} is {mod:d} and is greater than 5")
 elif mod == 0:
