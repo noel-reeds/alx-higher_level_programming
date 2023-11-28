109c34efafcfc59e4e6a65ba22227af103635068
Noel Reeds <noelreeds01@gmail.com>
2023-11-28 21:55:10 +0000

    fixed last digit issue for -ve number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 mod = abs(number) % 10
 if mod > 5:
     print(f"Last digit of {number:d} is {mod:d} and is greater than 5")
-if mod == 0:
+elif mod == 0:
     print(f"Last digit of {number:d} is {mod:d} and is 0")
-if mod < 6 and mod != 0:
+elif mod < 6 and mod != 0:
     print(f"Last digit of {number:d} is {mod:d} and is less than 6 and not 0")
