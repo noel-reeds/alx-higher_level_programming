6cf590dd0818956bb93d9d3cce3e7cf5e60c7efd
Noel Reeds <noelreeds01@gmail.com>
2023-11-28 21:25:34 +0000

    sorts to display last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+mod = number % 10
+if mod > 5:
+    print(f"Last digit of {number:d} is {mod:d} and is greater than 5")
+if mod == 0:
+    print(f"Last digit of {number:d} is {mod:d} and is 0")
+if mod < 6 and not 0:
+    print(f"Last digit of {number:d} is {mod:d} and is less than 6 and not 0")
