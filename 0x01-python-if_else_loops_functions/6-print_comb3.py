d7a31ce1b9beed65b739e0481c1a95b639d925f2
Noel Reeds <noelreeds01@gmail.com>
2023-11-30 21:16:20 +0000

    sorts to print combinations

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for n in range(0, 9):  # iterate over the first digit.
+    for m in range(1, 10):  # iterate over the second digit.
+        print("{}".format(n), end="")  # print the first digit.
+        print("{}, ".format(m), end="")  # print the second digit.
