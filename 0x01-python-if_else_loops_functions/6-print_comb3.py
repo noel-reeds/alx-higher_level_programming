a1035d5654fd9c8cbfac24cd81d9d8c14b7d6206
Noel Reeds <noelreeds01@gmail.com>
2023-11-30 21:42:18 +0000

    fixing next line character

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,7 @@
 for n in range(0, 9):  # iterate over the first digit.
     for m in range(1, 10):  # iterate over the second digit.
         print("{}".format(n), end="")  # print the first digit.
-        print("{}, ".format(m), end="")  # print the second digit.
+        print("{}".format(m), end="")  # print the second digit.
+        if n == 8 and m == 9:
+            continue
+        print("{}".format(", "), end="")
