2476afef26300020a30d4e42c7112d9fa5d1d026
Noel Reeds <noelreeds01@gmail.com>
2023-11-30 11:21:19 +0000

    fixed print of combinations

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99, 2):
-    print(i)
-    print(", ")
+for num in range(100):
+    if num != 99:
+        print('{}'.format(num), end=', ')
+    else:
+        print(num)
