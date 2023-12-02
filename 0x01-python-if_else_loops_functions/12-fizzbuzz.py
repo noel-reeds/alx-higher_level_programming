fa60147b8221b7c1fcc6307d7538deb577a2fa34
Noel Reeds <noelreeds01@gmail.com>
2023-12-02 10:46:19 +0000

    sorts to fix a case of both multiples

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,9 +2,11 @@
 def fizzbuzz():
     """Prints the numbers from 1 to 100 separated by a space"""
     for m in range(1, 101):
-        if m % 3 == 0:
-            print('{}'.format('Fizz'), end=' ')
+        if m % 3 == 0 and m % 5 == 0:
+            print('{}'.format('FizzBuzz'), end=' ')
         elif m % 5 == 0:
             print('{}'.format('Buzz'), end=' ')
+        elif m % 3 == 0:
+            print('{}'.format('Fizz'), end=' ')
         else:
             print('{}'.format(m), end=' ')
