c9f379f133adabe8b5f13cb67dbd38e1c5f59f33
Noel Reeds <noelreeds01@gmail.com>
2023-12-02 11:59:18 +0000

    checks for lower case

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     """Returns True for lower case and False otherwise"""
-    
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
