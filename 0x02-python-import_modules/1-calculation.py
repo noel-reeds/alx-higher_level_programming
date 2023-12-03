3a0c89ac0f2596d9888f5224d9741987946dfec8
Noel Reeds <noelreeds01@gmail.com>
2023-12-03 18:56:42 +0000

    sorted module not to run when imported

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,10 @@ add(a, b)
 sub(a, b)
 mul(a, b)
 div(a, b)
-print('{} + {} = {}'.format(a, b, add(a, b)))
-print('{} - {} = {}'.format(a, b, sub(a, b)))
-print('{} * {} = {}'.format(a, b, mul(a, b)))
-print('{} / {} = {}'.format(a, b, div(a, b)))
+print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+
+if __name__ == "__main__":
+    import sys
