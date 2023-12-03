a29960e13780f1ebd47c748e023d4f70d8543ac3
Noel Reeds <noelreeds01@gmail.com>
2023-12-03 17:52:39 +0000

    added imported module to the current dir

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def add(a, b):
-    res = a + b
-    return res
+from add_0 import add
+a = 1
+b = 2
+add(a, b)
+print('{} + {} + {}'.format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
