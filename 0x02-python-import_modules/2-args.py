0246c20c034bf7708441b7223d5842388491348c
Noel Reeds <noelreeds01@gmail.com>
2023-12-04 10:40:15 +0000

    handled number of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,8 @@
 import sys
 res = len(sys.argv)
 if res == 1:
-    print('0 arguments.')
+    print('{} arguments.'.format(res-1))
 elif res == 2:
-    print('1 argument')
-    print('{} {}'.format('1:', sys.argv[res-1]))
+    print('{} argument:'.format(res-1))
 else:
-    print('{} arguments'.format(res-1))
+    print('{} arguments:'.format(res-1))
