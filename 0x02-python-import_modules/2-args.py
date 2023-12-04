e93a5efaca80199434017e6b6ef72f8025280b47
Noel Reeds <noelreeds01@gmail.com>
2023-12-04 10:51:51 +0000

    solved for the first few args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,3 +7,10 @@ elif res == 2:
     print('{} argument:'.format(res-1))
 else:
     print('{} arguments:'.format(res-1))
+m = 1
+while m < res:
+    if res == 2:
+        print('{}: {}'.format(res-1, sys.argv[1]))
+    else:
+        pass
+    m = m + 1
