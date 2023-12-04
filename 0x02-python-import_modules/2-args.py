5d59f24c9fbb2874a6642bb034e3b1f99171a051
Noel Reeds <noelreeds01@gmail.com>
2023-12-04 11:14:46 +0000

    displays arguments passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,12 +5,11 @@ if res == 1:
     print('{} arguments.'.format(res-1))
 elif res == 2:
     print('{} argument:'.format(res-1))
+    print('{}: {}'.format(res-1, sys.argv[res-1]))
 else:
     print('{} arguments:'.format(res-1))
 m = 1
 while m < res:
-    if res == 2:
-        print('{}: {}'.format(res-1, sys.argv[1]))
-    else:
-        pass
+    if res > 2:
+        print("{}: {}".format(m, sys.argv[m]))
     m = m + 1
