b882a8161f483513859454e5c35f7233e0ae766a
Noel Reeds <noelreeds01@gmail.com>
2023-12-04 06:25:57 +0000

    A function corresponding to a Python bytecode

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+#3           0 LOAD_CONST               1 (98)
+#            3 LOAD_FAST                0 (a)
+#            6 LOAD_FAST                1 (b)
+#            9 BINARY_POWER
+#            10 BINARY_ADD
+#            11 RETURN_VALUE
+
+# corresponding function to the CPython bytecode
+
 def magic_calculation(a, b):
-    import dis
-    dis.dis(__name__)
+    return 98 + (a ** b)
