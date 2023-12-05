e4a7ed23ae3e2ad937d73a41a6ae702ddb69cfed
Noel Reeds <noelreeds01@gmail.com>
2023-12-05 00:13:01 +0000

    Fixed pycodestyle

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-	res = len(my_list)
-	for m in range(res):
-		print('{}'.format(my_list[m]))
-
+    res = len(my_list)
+    for m in range(res):
+        print('{}'.format(my_list[m]))
