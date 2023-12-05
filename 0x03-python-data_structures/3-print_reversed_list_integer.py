732eeb29203d474dfc214c630ab0b9c49440e7e8
Noel Reeds <noelreeds01@gmail.com>
2023-12-05 10:56:12 +0000

    Fixed for None list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    m = (len(my_list) - 1)
-    while m >= 0:
-        print('{:d}'.format(my_list[m]))
-        m = m - 1
+    if my_list != None:
+        m = (len(my_list) - 1)
+        while m >= 0:
+            print('{:d}'.format(my_list[m]))
+            m = m - 1
