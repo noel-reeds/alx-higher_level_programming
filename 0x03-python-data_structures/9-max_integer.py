7e2fc2f5f12bb3212f1723710d2239514e3614d8
Noel Reeds <noelreeds01@gmail.com>
2023-12-05 20:11:37 +0000

    Hurrayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    m = 0
+    res = (len(my_list)) - 1
     if my_list == []:
         return None
-    res = (len(my_list)) - 1
-    m = 0
-    if res == 0:
-        num = my_list[m]
+    elif res == 0:
+        return my_list[m]
     else:
         if my_list[m] < my_list[m+1]:
             num = my_list[m+1]
