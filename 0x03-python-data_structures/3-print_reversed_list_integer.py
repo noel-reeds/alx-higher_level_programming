e46ace586b31961ef9eed263271faba034ada9e2
Noel Reeds <noelreeds01@gmail.com>
2023-12-05 10:34:18 +0000

    specified an integer into str.format method

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     m = (len(my_list) - 1)
     while m >= 0:
-        print('{}'.format(my_list[m]))
+        print('{:d}'.format(my_list[m]))
         m = m - 1
