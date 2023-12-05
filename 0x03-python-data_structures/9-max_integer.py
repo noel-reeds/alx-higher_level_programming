6530d1781ebe0a1ce02f8a71989321423a818082
Noel Reeds <noelreeds01@gmail.com>
2023-12-05 19:45:52 +0000

    Trying to solve for when list has one item

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,14 +4,17 @@ def max_integer(my_list=[]):
         return None
     res = (len(my_list)) - 1
     m = 0
-    if my_list[m] < my_list[m+1]:
-        num = my_list[m+1]
-    else:
+    if res == 0:
         num = my_list[m]
-    while m < res:
-        if num < my_list[m+1]:
+    else:
+        if my_list[m] < my_list[m+1]:
             num = my_list[m+1]
         else:
-            num = num
-        m = m + 1
-    return num
+            num = my_list[m]
+        while m < res:
+            if num < my_list[m+1]:
+                num = my_list[m+1]
+            else:
+                num = num
+            m = m + 1
+        return num
