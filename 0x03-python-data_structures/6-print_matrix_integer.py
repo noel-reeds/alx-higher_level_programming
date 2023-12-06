d2d339ce0a0496a6bfa01718e26f939351122e44
Noel Reeds <noelreeds01@gmail.com>
2023-12-06 22:07:59 +0000

    prints a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for m in matrix:
         for n in m:
-            print("{:d}".format(n), end=" ")
+            if n != m[-1]:
+                print("{:d}".format(n), end=" ")
+            else:
+                print("{:d}".format(n), end="")
         print()
