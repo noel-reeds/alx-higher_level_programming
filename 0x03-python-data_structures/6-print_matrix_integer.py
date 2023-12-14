2ba2a3a8d0524aec5e28e2c708cbcb6f278e2d2a
Noel Reeds <noelreeds01.com>
2023-12-14 20:31:09 +0000

    updated to rows

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for m in matrix:
-        for n in m:
-            if n != m[-1]:
-                print("{:d}".format(n), end=" ")
+    for row in matrix:
+        for i in row:
+            if i != row[-1]:
+                print("{:d}".format(i), end=" ")
             else:
-                print("{:d}".format(n), end="")
+                print("{:d}".format(i), end="")
         print()
