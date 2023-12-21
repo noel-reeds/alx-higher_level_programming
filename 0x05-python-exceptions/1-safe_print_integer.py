4ec33345ac8c729c92677706dda1298f8047d742
Noel Reeds <noelreeds01.com>
2023-12-21 22:23:42 +0000

    Fixed a None edge case

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try:
-        print('{:d}'.format(value))
-        return True
-    except ValueError:
-        return False
+    if value is not None:
+        try:
+            print('{:d}'.format(value))
+            return True
+        except ValueError:
+            return False
