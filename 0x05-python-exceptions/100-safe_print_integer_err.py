c9eb5e5b56dc87ea1085c9b2078bc8bcb7e090fe
Noel Reeds <noelreeds01.com>
2023-12-22 13:24:18 +0000

    Fixes TypeError err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
-    if value is not None:
-        import sys
-        try:
-            print('{:d}'.format(value))
-            return True
-        except Exception as err:
-            sys.stderr.write("Exception: {}\n".format(err))
-            return False
+    import sys
+    try:
+        print('{:d}'.format(value))
+        return True
+    except (ValueError, TypeError) as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return False
