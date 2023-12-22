cf1050e750d4df55ccb635a2405e1601f6f56690
Noel Reeds <noelreeds01.com>
2023-12-22 13:07:29 +0000

    Fixes exception err messages

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,6 @@ def safe_print_integer_err(value):
         try:
             print('{:d}'.format(value))
             return True
-        except ValueError:
-            sys.stderr.write(
-                "Exception: Unknown format code 'd' for object of type 'str'\n"
-            )
+        except ValueError as err:
+            sys.stderr.write("Exception: {}\n".format(err))
             return False
