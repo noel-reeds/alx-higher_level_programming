2022e94ceb09fb83327ff7aa81526f1363d0e4c4
Noel Reeds <noelreeds01.com>
2023-12-22 12:22:33 +0000

    Fix for pycodestyle documentation

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,7 @@ def safe_print_integer_err(value):
             print('{:d}'.format(value))
             return True
         except ValueError:
-            sys.stderr.write("Exception: Unknown format code\
-            'd' for object of type 'str'\n")
+            sys.stderr.write(
+                "Exception: Unknown format code 'd' for object of type 'str'\n"
+            )
             return False
