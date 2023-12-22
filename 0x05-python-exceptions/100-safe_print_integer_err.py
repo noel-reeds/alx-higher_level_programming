0b9a3d416ac282ab841d97ffaafb8c338fd88422
Noel Reeds <noelreeds01.com>
2023-12-22 03:13:55 +0000

    Fixed pycodestyle

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
-    import sys
-    try:
-        print('{:d}'.format(value))
-        return True
-    except ValueError:
-        sys.stderr.write("Exception: Unknown format code 'd' for object of type 'str'\n")
-        return False
+    if value is not None:
+        import sys
+        try:
+            print('{:d}'.format(value))
+            return True
+        except ValueError:
+            sys.stderr.write("Exception: Unknown format code\
+            'd' for object of type 'str'\n")
+            return False
