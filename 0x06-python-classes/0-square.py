0ebd03060743d56110052c5ec4f5b30202721c5f
Noel Reeds <noelreeds01@gmail..com>
2023-12-25 11:24:50 +0000

    corrected module documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 class Square:
-    """A square is a four-sided shape with equal sides."""
+    """This ia an empty class
+    A square is a four-sided shape with equal sides.
+    """
     def __init__(self):
-        """Defines a square"""
+        """Initializes a square"""
         pass
