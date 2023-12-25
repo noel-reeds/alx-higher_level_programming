ab9e3cde0ffea12fa7acf6963fb954da7a7d7f9f
Noel Reeds <noelreeds01@gmail..com>
2023-12-25 12:57:19 +0000

    specified the type and limits of attr.

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This is a class representing a square
+"""
+
+
+class Square:
+    """
+    This is an empty class, defines a square
+    A square is a four-sided shape with equal sides.
+    """
+    def __init__(self, size=0):
+        """
+        The __init__ method is the constructor for the class
+        Logic for the method will be added later.
+
+        Args:
+            size(int): The size of the obj. square.
+        """
+        self.__size = size
+        if size is not int:
+            print("size must be an integer")
+        elif size < 0:
+            print("size must be >= 0")
+        else:
+            self.__size = size
