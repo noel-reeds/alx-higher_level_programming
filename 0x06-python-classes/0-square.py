06668248e303752dfe54f785bb9958b827822254
Noel Reeds <noelreeds01@gmail..com>
2023-12-25 11:57:25 +0000

    edited module docstrings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-
 """
 This is a class representing a square
+A square is a four-sided shape with equal sides
 """
 
 
 class Square:
-    """
-    This is an empty class representing a square.
-    Explanation:
-    A square is a four-sided shape with equal sides.
-    """
     def __init__(self):
         """
-        Instantiation and initialization happens here.
-        Note:
-            The __init__ method is the constructor for the class
-            The module is currently blank, will be extended later with
-            additional logic.
+        The __init__ method is the constructor for the class
+        Logic for the method will be added later.
         """
         pass
