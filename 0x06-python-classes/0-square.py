4eec8477bbd7864908297be23dc699848965c1b0
Noel Reeds <noelreeds01@gmail..com>
2023-12-25 09:41:45 +0000

    corrected module documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """This class defines a square"""
-    def __init__(self):
-        """This module defines an instance of class Square.
-
-        Args:
-            The module takes the obj. as argument as required.
-
-        Returns:
-            bool: True for success, False otherwise.
-        """
-        pass
+    """A square is a four-sided shape with equal sides."""
+    pass
