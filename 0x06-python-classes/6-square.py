7d57ee5d7b3f79247a910f12410cf2eddc202ffb
Noel Reeds <noelreeds01@gmail..com>
2023-12-28 21:20:40 +0000

    removed repetition, involved setter and getter from the init method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,25 +15,8 @@ class Square:
             size(int): The size of the obj. square.
             position(tuple): cordinates of obj. square
         """
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-        """
-        position(tuple): cordinates of obj. square
-        """
-        if type(position) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(position[0]) is not int or type(position[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = position
+        self.size = size
+        self.position = position
 
     """
     This is the area method of the class Square.
