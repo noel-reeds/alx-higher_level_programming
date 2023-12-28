49c86002368d163a7878b2fd4557ed8944098988
Noel Reeds <noelreeds01@gmail..com>
2023-12-28 08:45:31 +0000

    Fixed less than edge case for position tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,6 +30,8 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(position[0]) is not int or type(position[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
 
@@ -99,6 +101,8 @@ class Square:
         elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
