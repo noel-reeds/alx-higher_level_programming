65f45acd504dff1b603b1794ff43dc39827529be
Noel Reeds <noelreeds01@gmail..com>
2023-12-30 22:12:48 +0000

    sorts a linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -33,16 +33,16 @@ class Node:
     """
     @property
     def next_node(self):
-        return next_node
+        return self.__next_node
     """
     Sets a private instance attr. next_node
     """
     @next_node.setter
     def next_node(self, value):
-        if value is None:
-            raise TypeError("next_node must be a Node object")
-        else:
+        if value is None or type(value) is Node:
             self.__next_node = value
+        else:
+            raise TypeError("next_node must be a Node object")
 
 
 """
@@ -62,25 +62,35 @@ class SinglyLinkedList:
     """
     def sorted_insert(self, value):
         new_node = Node(value)  # create a Node.
-        if self.__head is None:  # empty linked list.
-            new_node.next_node = self.__head  # new node takes head ptr addr.
-            self.__head = new_node  # head ptr points to new node.
+        if self.head is None:  # empty linked list.
+            new_node.next_node = self.head  # new node takes head ptr addr.
+            self.head = new_node  # head ptr points to new node.
         else:  # linked list is not empty.
-            temp = self.__head  # assign a temp node to iterate.
-            while temp.next_node is not None:
-                if temp.next_node.data > value:
-                    new_node.next_node = temp.next_node
-                    temp.next_node = new_node
+            temp = self.head  # assign a temp node to iterate.
+            if self.head.data > value:
+                new_node.next_node = self.head
+                self.head = new_node
+            else:
+                while temp.next_node is not None:
+                    if temp.next_node.data > value:
+                        new_node.next_node = temp.next_node
+                        temp.next_node = new_node
+                        break
+                    else:
+                        temp = temp.next_node
                 else:
-                    temp = temp.next_node
-            temp.next_node = new_node
+                    temp.next_node = new_node
 
     """
     Prints a singly linked list to stdout
     """
     def display_linkedlist(self):
-        if self.__head is None:
-            temp = self.__head
-            while tempt is not None:
+        if self.head is not None:
+            temp = self.head
+            while temp is not None:
                 print(temp.data)
                 temp = temp.next_node
+
+    def __str__(self):
+        self.display_linkedlist()
+        return ""
