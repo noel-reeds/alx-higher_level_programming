0d98ac548c10ff96d55a8b33f4e4b5b4ed191c4a
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2023-12-31 16:11:52 +0000

    sorts to fix a None edge case

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    if value is not None:
+        try:
+            print('{:d}'.format(value))
+            return True
+        except Exception:
+            return False
+    else:
+        return False
