d3d65b38d435343c6c44b0afe663a908cc1ab0c2
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-01-06 12:54:39 +0000

    Fixed to display any type of symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
         rectangle = ""
         for m in range(self.__height):
             for n in range(self.__width):
-                rectangle += self.print_symbol
+                rectangle += str(self.print_symbol)
             if m < self.__height - 1:
                 rectangle += "\n"
         return rectangle
