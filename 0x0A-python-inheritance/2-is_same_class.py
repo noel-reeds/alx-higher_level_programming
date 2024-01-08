78d23c67e6f3417f8545d4ddec65ea8f7dfb11ef
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-01-08 19:27:44 +0000

    checks for instance of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""""This module checks for instantiation"""
+
+
+def is_same_class(obj, a_class):
+    """returns True for an instance and False otherwise"""
+    if isinstance(obj, a_class):
+        return True
+    return False
