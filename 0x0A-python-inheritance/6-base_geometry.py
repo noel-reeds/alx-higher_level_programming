0e5ccbcfc69805a750a16df228a1553513cddd2a
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-01-09 06:13:40 +0000

    added an instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is an empty class"""
+
+
+class BaseGeometry:
+    """public instance method"""
+    def area(self):
+        raise Exception('area() is not implemented')
