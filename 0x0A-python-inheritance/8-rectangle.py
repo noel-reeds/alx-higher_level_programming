8105eaa037246c899e2f22a7e4804cd0ce103f06
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-01-09 09:18:40 +0000

    Fixed err

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,7 +13,6 @@ class BaseGeometry:
             raise TypeError('{} must be an integer'.format(name))
         elif value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
-        self.value = value
 
 
 class Rectangle(BaseGeometry):
