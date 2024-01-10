acfa3a7ca98d5a17ffe5c6885d1c73ee53a5bc5e
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-01-10 13:06:46 +0000

    creates an obj. from JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""converts a JSON file into an obj.."""
+import json
+
+
+def load_from_json_file(filename):
+    """converts a 'JSON text file' to a Python object.."""
+    with open(filename, mode="r", encoding="utf-8") as myFile:
+        python_obj = json.load(myFile)
