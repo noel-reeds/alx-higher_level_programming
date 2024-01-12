a0d068c306999913d7667336d383acd7406f9d20
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-01-12 04:09:43 +0000

    Fixed pycodestyle validation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """script, adds args to a Python list, saves them in a file"""
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 import json
 import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
