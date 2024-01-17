9cbe24374fc3a7256044adc92f75589e9e3be6a4
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-01-17 23:22:48 +0000

    prints a square of '#'

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module prints a square with the character "#"
+"""
+
+
+def print_square(size):
+    """
+    Prints a square of symbol, '#' to the stdout..
+    
+    Args:
+        size(int):
+
+    Returns:
+        A square of '#'
+    """
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if isinstance(size, float) and size < 0:
+        raise TypeError('size must be an integer')
+
+    for m in range(size):
+        for m in range(size):
+            print('#', end="")
+        print()
