a1c6f5e0a7521f4795ba1463ab3e2d7d4eacaf90
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-01-17 10:29:56 +0000

    sorts to fix both test cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""Module adds two integers.."""
+
+
 def add_integer(a, b=98):
+    """adds two integers and return sum.."""
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    else:
+        return int(a + b)
