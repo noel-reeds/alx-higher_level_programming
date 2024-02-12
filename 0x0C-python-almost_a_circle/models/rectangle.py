bdac53a44fe8c3e1914c893f1b2a9c9cca6b213f
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-02-12 20:29:37 +0000

    validated attrs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,6 +20,10 @@ class Rectangle(Base):
     """sets the width attribute"""
     @width.setter
     def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     """retrieves height attribute"""
@@ -30,6 +34,10 @@ class Rectangle(Base):
     """sets the height attribute"""
     @height.setter
     def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        elif value <= 0:
+            raise ValueError('height must be > 0')
         self.__height = value
 
     """retrives y attribute"""
@@ -40,6 +48,10 @@ class Rectangle(Base):
     """sets y attribute"""
     @y.setter
     def y(self, value):
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        elif value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = value
 
     """retrieves x attribute"""
@@ -50,4 +62,8 @@ class Rectangle(Base):
     """validates x attributes"""
     @x.setter
     def x(self, value):
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        elif value < 0:
+            raise ValueError('x must be >= 0')
         self.__x = value
