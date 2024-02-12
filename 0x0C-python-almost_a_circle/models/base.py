c701f79986d5a9f3eecdb78868f888b828d5eaef
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-02-12 15:15:09 +0000

    base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,8 +6,10 @@ class Base:
     """This is a Base class"""
     __nb_objects = 0
 
+    """Initializes instance objects"""
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            self.id = type(self).__nb_objects + 1
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects
