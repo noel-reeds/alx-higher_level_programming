f3458cfdc9222059970c959b83bb7652240a7361
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-02-13 14:04:32 +0000

    added unittests

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Tests '6-max_integer' module
+"""
+import unittest
+from 6-max_integer import max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    Define tests methods, each to start with 'test'
+    """
+    def test_empty(self):
+        """Returns None for an empty list"""
+        self.assertIsNone(list)
+
+    def test_values(self):
+        """Checks for values of list to be int"""
+        self.assertIsInstance(list[i], int)
