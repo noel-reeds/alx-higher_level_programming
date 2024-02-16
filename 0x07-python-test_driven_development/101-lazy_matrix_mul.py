09494b53cee3af74c8916eca51d03b789d4451c7
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-02-16 10:26:12 +0000

    lazy matrix multiply

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Lazy matrix multiplication
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    m_A = np.matrix(m_a)
+    m_B = np.matrix(m_b)
+
+    res = m_A * m_B
+    return res
