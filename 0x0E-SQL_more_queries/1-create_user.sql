330358dbebf8a78df826b23865dc34c648614308
altoid <noelreeds01@gmail.com>
2024-02-29 05:58:09 +0000

    creates a user and grands privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates a user and grants all privileges
 -- sql query syntax to create a user
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES TO user_0d_1@localhost ON *.*;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
