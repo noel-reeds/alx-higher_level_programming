25318ac9dc778bfdcc4b35ca46115c6b72d2b8a1
altoid <noelreeds01@gmail.com>
2024-03-03 22:11:19 +0000

    serializes objects into a JSON file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,21 @@ class Base:
             return json_str
         else:
             return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes JSON string to a file"""
+        if list_objs:
+            list_dicts = []
+            for obj in list_objs:
+                obj_dictionary = obj.to_dictionary()
+                list_dicts.append(obj_dictionary)
+            json_str = cls.to_json_string(list_dicts)
+            with open("Rectangle.json", mode="w", encoding="utf-8") as MyFile:
+                MyFile.write(json_str)
+        else:
+            list_objs = []
+            obj_dictionary = list_objs.to_dictionary()
+            json_str = to_json_string(obj_dictionary)
+            with open("Rectangle.json", mode="w", encoding="utf-8") as MyFile:
+                MyFile.write(json_str)
