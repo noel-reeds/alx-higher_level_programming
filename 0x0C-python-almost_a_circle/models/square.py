c28e8a6e23ca424fc42e2b2185d2b5e9dd142b6b
altoid <noelreeds01@gmail.com>
2024-03-03 15:40:57 +0000

    removed new attr created

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Init method"""
         super().__init__(size, size, x, y, id)
-        self.size = size
+        self.__width = self.__height = size
 
     def __str__(self):
         """custom str method"""
@@ -17,7 +17,7 @@ class Square(Rectangle):
     @property
     def size(self):
         """retrieves the size attrs"""
-        return self.__size
+        return self.__width
 
     @size.setter
     def size(self, value):
@@ -26,4 +26,5 @@ class Square(Rectangle):
             raise TypeError('width must be an integer')
         elif value <= 0:
             raise ValueError('width must be > 0')
-        self.__size = value
+        self.__width = value
+        self.__height = value
