38cf4ce12d69e44e4dd6000f8c00c1480ce36499
altoid <noelreeds01@gmail.com>
2024-03-04 12:23:59 +0000

    sorts to fix 'mix objects' edge case

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,9 +19,9 @@ class Base:
         """dictionary rep of an instance to JSON string"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        if (type(list_dictionaries) != list or not
-                all(type(var) == dict for var in list_dictionaries)):
-            raise TypeError("mst_dictionaries must be a list of dictionaries")
+        if not isinstance(list_dictionaries, list) or\
+                not all(isinstance(var, dict) for var in list_dictionaries):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
 
     @classmethod
