cd1c2646464163bb90458e4c036f6ddd33108e23
altoid <noelreeds01@gmail.com>
2024-03-13 14:33:32 +0000

    detects commandline args passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 if (argv.length === 2) {
-  console.log('No argument found');
+  console.log('No argument');
 } else if (argv.length === 3) {
   console.log('Argument found');
 } else if (argv.length > 3) {
