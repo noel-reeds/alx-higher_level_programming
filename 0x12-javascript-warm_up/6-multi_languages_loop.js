5b52f4f0912eed3c6ee7ff52f12795613d40c302
altoid <noelreeds01@gmail.com>
2024-03-13 20:51:19 +0000

    JS while loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lenArray = myArray.length;
+let m = 0;
+while (m < lenArray) {
+  console.log(`${myArray[m]}`);
+  m++;
+}
