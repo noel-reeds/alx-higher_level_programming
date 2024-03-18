49c22baffbe90826a15573d1628bd4028ff8e1f2
altoid <noelreeds01@gmail.com>
2024-03-18 13:11:01 +0000

    sorted semistandard validation

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
-      return "Rectangle {}";
+      return 'Rectangle {}';
     }
     this.width = w;
     this.height = h;
