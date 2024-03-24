7cc487f8568c538245677822c936f8464b82099a
altoid <noelreeds01@gmail.com>
2024-03-24 13:12:30 +0000

    returns states matching state name seached

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""displays all values in the states table """
+import MySQLdb
+from sys import argv
+db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], search=argv[4])
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE %search%")
+states = cur.fetchall()
+for state in states:
+    print(state)
+
+if __name__ == "__main__":
+    pass
