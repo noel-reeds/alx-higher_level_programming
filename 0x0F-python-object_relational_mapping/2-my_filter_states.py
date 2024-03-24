29efdd1be326bad1f28f2d38f13defafdf3326e7
altoid <noelreeds01@gmail.com>
2024-03-24 14:36:29 +0000

    sort str format method

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,7 +5,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %search%")
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}'".format(argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
