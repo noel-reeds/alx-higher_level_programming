9bd78ef76d27e7392ffe945ac8b52024b766b502
altoid <noelreeds01@gmail.com>
2024-03-24 14:10:15 +0000

    changed to executable

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """displays all values in the states table """
-db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], search=argv[4])
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE %search%")
-states = cur.fetchall()
-for state in states:
-    print(state)
-
+import MySQLdb
+from sys import argv
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %search%")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
