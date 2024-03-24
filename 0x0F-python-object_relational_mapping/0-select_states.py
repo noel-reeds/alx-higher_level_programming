ada450cb454c9dd25dab5f4294d788991fd4ff8b
altoid <noelreeds01@gmail.com>
2024-03-24 11:19:18 +0000

    sorts pycodestyle validation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,14 +2,14 @@
 import MySQLdb
 import sys
 # creates a connection
-db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 # creates a cursor object
 cur = db.cursor
 # uses cursor object to execute sql queries
 cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 states = cur.fetchall()
 for state in states:
-	print(state)
+    print(state)
 
 if __name__ == "__main__":
-	import MySQLdb
+    import MySQLdb
