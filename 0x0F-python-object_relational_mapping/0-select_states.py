80da233bdebdd90215ec363a0cbf6af963ed0221
altoid <noelreeds01@gmail.com>
2024-03-24 11:12:11 +0000

    lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+# creates a connection
+db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+# creates a cursor object
+cur = db.cursor
+# uses cursor object to execute sql queries
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+states = cur.fetchall()
+for state in states:
+	print(state)
+
+if __name__ == "__main__":
+	import MySQLdb
