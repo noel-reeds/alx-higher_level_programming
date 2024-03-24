cf489744deb479339440b2ea7b53177b00d5059d
altoid <noelreeds01@gmail.com>
2024-03-24 13:29:54 +0000

    code fails when imported

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """displays all values in the states table """
-import MySQLdb
-from sys import argv
 db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], search=argv[4])
 cur = db.cursor()
 cur.execute("SELECT * FROM states WHERE name LIKE %search%")
@@ -10,4 +8,5 @@ for state in states:
     print(state)
 
 if __name__ == "__main__":
-    pass
+    import MySQLdb
+    from sys import argv
