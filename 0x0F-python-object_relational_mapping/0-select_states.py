ae7e209a026b620e3c1e1f9035c8eebfe93f3390
altoid <noelreeds01@gmail.com>
2024-03-24 11:22:10 +0000

    module commented

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+# lists states in a database
 import MySQLdb
 import sys
-# creates a connection
+
 db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-# creates a cursor object
+
 cur = db.cursor
-# uses cursor object to execute sql queries
+
 cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 states = cur.fetchall()
 for state in states:
