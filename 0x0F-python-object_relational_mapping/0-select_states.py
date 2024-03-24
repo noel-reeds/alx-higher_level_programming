987cb7b9eb2ff4b36a06a207c52c0faad313816c
altoid <noelreeds01@gmail.com>
2024-03-24 11:37:44 +0000

    lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""lists states in a database"""
-import MySQLdb
+"""Lists states in a database"""
 import sys
-
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
-cur = db.cursor
-
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-states = cur.fetchall()
-for state in states:
-    print(state)
-
+import MySQLdb
 if __name__ == "__main__":
-    import MySQLdb
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
