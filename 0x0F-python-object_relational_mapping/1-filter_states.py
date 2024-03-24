a5eb4d7db761667728d2c80ced826ba12d574527
altoid <noelreeds01@gmail.com>
2024-03-24 12:57:56 +0000

    lists all states with a name starting with N (upper N) from the database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,9 +3,9 @@
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%'")
     states = cur.fetchall()
     for state in states:
         print(state)
