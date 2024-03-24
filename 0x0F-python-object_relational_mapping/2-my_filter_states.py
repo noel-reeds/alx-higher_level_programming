03a6a48e12bc9fb34b40e92af44661a74779e734
altoid <noelreeds01@gmail.com>
2024-03-24 14:53:20 +0000

    displays all values in the states table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,10 +2,12 @@
 """displays all values in the states table """
 import MySQLdb
 from sys import argv
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}'".format(argv[4]))
-    states = cur.fetchall()
-    for state in states:
-        print(state)
+db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE BINARY name LIKE '{}'".format(argv[4]))
+states = cur.fetchall()
+for state in states:
+    print(state)
+
+if __name__ != "__main__":
+    pass
