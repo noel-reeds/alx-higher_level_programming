3b8ffd51add95a0b86c45476083807512532c7d7
altoid <noelreeds01@gmail.com>
2024-03-25 05:31:43 +0000

    Lists all cities from the database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         port=3306
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    cur.execute("SELECT * FROM cities JOIN states ON cities.state_id=states.id")
     cities = cur.fetchall()
     for city in cities:
         print(city)
