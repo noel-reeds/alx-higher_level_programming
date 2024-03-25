852a6979928027fe191e4768c1d00a465acaf942
altoid <noelreeds01@gmail.com>
2024-03-25 11:53:09 +0000

    Lists cities in specified state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all cities in the database"""
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
+    cur = db.cursor()
+    state = argv[4]
+    cur.execute(
+        "SELECT cities.name "
+        "FROM cities "
+        "LEFT JOIN states "
+        "ON cities.state_id=states.id "
+        "WHERE states.name = %s", (state,)
+    )
+    cities = cur.fetchall()
+    for city in cities:
+        print(city)
