09f22292828d932e83635f64c0be0b6cb0138db0
altoid <noelreeds01@gmail.com>
2024-03-26 20:22:17 +0000

    First state model using SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""First state model"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine, Sequence
+
+Base = declarative_base()
+engine = create_engine('mysql+mysqldb://:altoid:1234@localhost/3306/mydb')
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, Sequence('user_id_seq', not None))
+    name = Column(String(128), not None)
