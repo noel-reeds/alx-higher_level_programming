db06c053a7114950243c011996535c506c7ec51c
altoid <noelreeds01@gmail.com>
2024-03-26 20:44:28 +0000

    sorts 'module documented' error

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,5 +15,5 @@ class State(Base):
     """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, Sequence('user_id_seq'), not None, doc="The primary key identifier for the state.")
-    name = Column(String(128), not None, doc="The name of the state.")
+    id = Column(Integer, primary_key=True, Sequence('user_id_seq'), nullable=False)
+    name = Column(String(128), nullable=False)
