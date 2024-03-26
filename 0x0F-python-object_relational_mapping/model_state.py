bb9a53d0f64139ba2605974bda3b500c72452d5c
altoid <noelreeds01@gmail.com>
2024-03-26 20:37:36 +0000

    module documented

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""First state model"""
+"""
+First state model
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine, Sequence
 
 Base = declarative_base()
 engine = create_engine('mysql+mysqldb://:altoid:1234@localhost/3306/mydb')
 
-"""State Class inherits"""
+
 class State(Base):
+    """
+    State class inherits from Base
+    """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, Sequence('user_id_seq', not None))
-    name = Column(String(128), not None)
+    id = Column(Integer, primary_key=True, Sequence('user_id_seq'), not None, doc="The primary key identifier for the state.")
+    name = Column(String(128), not None, doc="The name of the state.")
