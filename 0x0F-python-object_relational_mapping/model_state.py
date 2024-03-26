0acadbbe4b57115e726046c083f0ffdcad36067e
altoid <noelreeds01@gmail.com>
2024-03-26 21:55:32 +0000

    sorts 'module documented' err

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,15 +5,21 @@ First state model
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine, Sequence
 
-Base = declarative_base()
-engine = create_engine('mysql+mysqldb://:altoid:1234@localhost/3306/mydb')
+if __name__ == "__main__":
+    Base = declarative_base()
+    engine = create_engine('mysql+mysqldb://:altoid:1234@localhost/3306/mydb')
 
 
-class State(Base):
-    """
-    State class inherits from Base
-    """
-    __tablename__ = 'states'
+    class State(Base):
+        """
+        State class inherits from Base
+        """
+        __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, Sequence('user_id_seq'), nullable=False)
-    name = Column(String(128), nullable=False)
+        id = Column(
+                Integer,
+                Sequence('user_id_seq'),
+                primary_key=True,
+                nullable=False
+         )
+        name = Column(String(128), nullable=False)
