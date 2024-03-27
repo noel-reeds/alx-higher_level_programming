93b8581539d9116978937b12a46c5b5d6cb53095
altoid <noelreeds01@gmail.com>
2024-03-27 07:29:26 +0000

    Lists all State objects that contain the letter 'a' from the database

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+from sqlalchemy import create_engine
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+if len(sys.argv) != 4:
+    print("Error: Please provide enough arguments.")
+    exit(1)
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+try:
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(username, password, database))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like('%a%'))
+    if (states):
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+    else:
+        print('Nothing')
+except engine.Error as err:
+    print("Error:", err)
+engine.dispose()
