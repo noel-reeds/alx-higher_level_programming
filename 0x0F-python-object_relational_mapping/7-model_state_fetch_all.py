2a0f3683c12429b1cda6430896c8fe9a2aa1f756
altoid <noelreeds01@gmail.com>
2024-03-27 07:05:14 +0000

    code fail when imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,23 +4,24 @@ from sqlalchemy import create_engine
 from model_state import State, Base
 from sqlalchemy.orm import sessionmaker
 
-if len(sys.argv) != 4:
-    print("Error: Please provide enough arguments.")
-    exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Error: Please provide enough arguments.")
+        exit(1)
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-try:
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'
-            .format(username, password, database))
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states = session.query(State).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-except engine.Error as err:
-    print("Error:", err)
-engine.dispose()
+    try:
+        engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost/{}'
+                .format(username, password, database))
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        states = session.query(State).all()
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+    except engine.Error as err:
+        print("Error:", err)
+    engine.dispose()
