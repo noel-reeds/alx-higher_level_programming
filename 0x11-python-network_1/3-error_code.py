38dbc0a9d2b60e34e5211b7066e5e2754f1161e9
altoid <noelreeds01@gmail.com>
2024-04-15 22:48:13 +0000

    returns http_code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Exceptions"""
+
+import urllib.request
+import sys
+if __name__ == "__main__":
+    try:
+        req = urllib.request.Request(sys.argv[1])
+        with urllib.request.urlopen(req) as response:
+            res = response.read()
+    except urllib.error.HTTPError as err:
+        print('Error code: {}'.format(err.code))
