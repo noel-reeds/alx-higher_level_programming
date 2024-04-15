0cb38d07fd9892522f34c043b0b249284101e8be
altoid <noelreeds01@gmail.com>
2024-04-15 22:28:48 +0000

    sorts module documented err

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """A POST request with urllib"""
+
 import urllib.request
 import urllib.parse
 import sys
 if __name__ == "__main__":
-    data = {'email': sys.argv[2])
+    data = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(data)
     data = data.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
