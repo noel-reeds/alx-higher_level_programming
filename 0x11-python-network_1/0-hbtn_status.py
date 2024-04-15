573c0f83895ff7239ab1c63c4541c1c069558e0f
altoid <noelreeds01@gmail.com>
2024-04-15 14:22:04 +0000

    display body of a response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import urllib
+import urllib.request
 req_object = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req_object) as req_page:
-    fetched_page = req_page.read()
+with urllib.request.urlopen(req_object) as req:
+    page = req.read()
+print('Body response:\n\t- type: {}\n\t- content: {}\n\t- \
+utf8 content: {}'.format(type(page), page, page.decode('utf-8')))
