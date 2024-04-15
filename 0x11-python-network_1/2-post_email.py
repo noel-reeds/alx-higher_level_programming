9b24dbdc727016d81aae3a08a095a9f835d96626
altoid <noelreeds01@gmail.com>
2024-04-15 22:24:38 +0000

    code not executed when imported

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """A POST request with urllib"""
 import urllib.request, sys, urllib.parse
-data = {'email': sys.argv[2])
-data = urllib.parse.urlencode(data)
-data = data.encode('ascii')
-req = urllib.request.Request(sys.argv[1], data)
-with urllib.request.urlopen(req) as response:
-    new_response = response.read()
-print(new_response.decode('utf-8'))
+if __name__ == "__main__":
+    data = {'email': sys.argv[2])
+    data = urllib.parse.urlencode(data)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        new_response = response.read()
+    print(new_response.decode('utf-8'))
 
