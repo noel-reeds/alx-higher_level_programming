712bf81b8775a5a7394b8fb52ce51195cacc212b
altoid <noelreeds01@gmail.com>
2024-04-16 14:47:13 +0000

    updated exception handling

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,6 +6,6 @@ import requests
 if __name__ == "__main__":
     try:
         req = requests.get(sys.argv[1])
-    except HTTPError as err:
+    except requests.exceptions.RequestException as err:
         if req.status_code >= 400:
             print('Error code: {}'.format(req.status_code))
