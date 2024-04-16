f9f8aae886d7bf5b5106c144cdbcb026081a78b0
altoid <noelreeds01@gmail.com>
2024-04-16 14:48:56 +0000

    okay..this

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     try:
         req = requests.get(sys.argv[1])
     except requests.exceptions.RequestException as err:
-        if req.status_code >= 400:
+        if err.status_code >= 400:
             print('Error code: {}'.format(req.status_code))
