e480611623e6bd6b323e68341aecc1b39917f009
altoid <noelreeds01@gmail.com>
2024-04-17 11:11:57 +0000

    sorts pycodestyle

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,7 @@ import sys
 import requests
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        payload={'q': ""}
+        payload = {'q': ""}
     else:
         payload = {'q': sys.argv[1]}
         req = requests.post('http://0.0.0.0:5000/search_user', payload)
@@ -14,7 +14,6 @@ if __name__ == "__main__":
                 req_dict = eval(req.text)
                 print('[{}] {}'.format(req_dict['id'], req_dict['name']))
             else:
-                print('No result')
-        
+                print('No result')        
         else:
-            print('Not a valid JSON')                    
+            print('Not a valid JSON')
