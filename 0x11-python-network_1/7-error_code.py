f745761dd2c752a1b338d33f97f4117f8e96b6f4
altoid <noelreeds01@gmail.com>
2024-04-17 01:07:00 +0000

    err code greater than expected

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,8 +4,6 @@
 import sys
 import requests
 if __name__ == "__main__":
-    try:
-        req = requests.get(sys.argv[1])
-    except requests.exceptions.RequestException as err:
-        if err.status_code >= 400:
-            print('Error code: {}'.format(req.status_code))
+    req = requests.get(sys.argv[1])
+    if req.status_code >= 400:
+        print('Error code: {}'.format(req.status_code))
