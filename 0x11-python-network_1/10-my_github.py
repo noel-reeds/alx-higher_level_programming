3b00b12e789c5ca42de1952a56ccecca22234180
altoid <noelreeds01@gmail.com>
2024-04-18 01:51:18 +0000

    basic authentication

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,9 +7,10 @@ import json
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    req = requests.get('https://api.github.com/user', auth=(username, password))
+    req = \
+        requests.get('https://api.github.com/user', auth=(username, password))
     if req.status_code == 200:
         user = json.loads(req.text)
         print(user['id'])
     else:
-        print('Err: {}'.format(req.status_code))
+        print('None')
