cf889b0d256dd6e234d9f9afb6d26ad226962ae1
altoid <noelreeds01@gmail.com>
2024-04-19 07:45:51 +0000

    retrieves last 10 commits of a github user

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Lists commits of a user's repository"""
+
+import sys
+import requests
+if __name__ == "__main__":
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    api = f"https://api.github.com/repos/{owner_name}/\
+{repository_name}/commits"
+    commits = requests.get(api)
+    commits = commits.json()
+    for m in range(10):
+        print("{}: {}".format(commits[m].get('sha'), commits[m].get('commit')['author']['name']))
