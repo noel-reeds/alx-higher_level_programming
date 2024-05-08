023dd0fb5426be7e107f61766a4aafaebe5d8788
altoid <noelreeds01@gmail.com>
2024-05-08 05:12:19 +0000

    returns the status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+const request = require('request');
+request.get(process.argv[2], function (error, response) {
+  if (error) { console.error(error); } else { console.log('code:', response.statusCode); }
+});
